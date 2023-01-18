acca23dc1c363cc41b3006bb92106b106765425e
DamarChattman <damarchattman@gmail.com>
2023-01-18 12:03:53 -0800

    final commit

--- server/app.js
+++ server/app.js
@@ -4,35 +4,38 @@
 //return info gotten from API to user 
 //once info is pulled from API push it into the storage array  
 
-var express = require('express');
+const express = require('express');
 const axios = require('axios');
 const morgan = require('morgan');
-var titleQueries = {}
+const titleQueries = {}
 
-//napp.use(morgan('dev'))
-var app = express();
+
+const app = express();
+app.use(morgan('dev'))
 app.get('/', (req, res)=>{
     let path = req.url
+    console.log(req.url)
     if (path === '/'){
-        res.send('Hello')
+        console.log('Hello')
     }
     if(titleQueries[path]){
         console.log('getting movie from titleQueries')
-        return res.json(titleQueries[path])
+        res.json(titleQueries[path])
     }
     axios
-    .get('https://www.omdbapi.com' + path + '&apikey=85f9dc34')
+    .get('https://www.omdbapi.com' + path + '&apikey=5f0280d0')
     .then(function (response) {
         var data = response.data
         titleQueries[path] = data
         res.status(200).json(data)
     })
+    
     .catch(function(error){
         res.status(200).json(error.message)
     })
     
 });
-
+//app.use(morgan('dev'))
 // When making calls to the OMDB API make sure to append the '&apikey=8730e0e' parameter
 
 module.exports = app;
